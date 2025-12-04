9b2d1a7793d49b4d9306d21501e97ff03c4f51e5
Tarik Eshaq <tarikeshaq@gmail.com>
2025-12-03 23:47:28 -0800

    are we functional yet

--- bin/day4.ml
+++ bin/day4.ml
@@ -1,11 +1,7 @@
-let make_matrix lines =
-    Util.make_matrix_of lines (fun c ->
-        match c with
-            | '@' -> 1
-            | _ -> 0
-    )
+let make_matrix lines = Util.make_matrix_of lines Fun.id 
     
 let dirs = [(0, 1); (0, -1); (1, 0); (1, -1); (1, 1); (-1, 0); (-1, 1); (-1, -1)] 
+
 let matrix_indices n m =
     let ( let* ) xs f = List.concat_map f xs in
     let* i = List.init n Fun.id in
@@ -15,18 +11,13 @@ let matrix_indices n m =
 let find_removable matrix =
     let n = Array.length matrix in
     let m = Array.length matrix.(0) in
-    let valid i j (dirI, dirJ) =
-        let i = dirI + i in
-        let j = dirJ + j in
-        i >= 0 &&
-        i < n &&
-        j >= 0 && 
-        j < m &&
-        matrix.(i).(j) = 1
+    let valid i j (di, dj) =
+        let ni = di + i in
+        let nj = dj + j in
+        ni>= 0 && ni < n && nj >= 0 && nj < m && matrix.(ni).(nj) = '@'
     in
     List.filter (fun (i, j) ->
-        matrix.(i).(j) = 1 &&
-        List.find_all (valid i j) dirs |> List.length |> (fun num -> num < 4)
+        matrix.(i).(j) = '@' && List.filter (valid i j) dirs |> List.length < 4 
     ) (matrix_indices n m) 
     
 let part1 lines = make_matrix lines |> find_removable |> List.length
@@ -34,10 +25,10 @@ let part1 lines = make_matrix lines |> find_removable |> List.length
 let part2 lines =
     let rec remove acc matrix =
         let removable = find_removable matrix in
-        if (List.length removable = 0) then acc
-        else begin
-            List.iter (fun (i, j) -> matrix.(i).(j) <- 0 ) removable;
-            remove (acc + List.length removable) matrix
-        end
+        match removable with
+        | [] -> acc
+        | _ -> 
+         List.iter (fun (i, j) -> matrix.(i).(j) <- '.' ) removable;
+         remove (acc + List.length removable) matrix
     in
     make_matrix lines |> remove 0
